a31d3e56e26ba5053cbd417b1f706400eb7f026a
公言山 <1573580882@qq.com>
2019-04-25 17:11:15 +0800

    html模板

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/templatesJs', express.static(path.join(__dirname, 'templatesJs')));
 
 // 自定义中间件
 app.use(tokenMiddleware);
